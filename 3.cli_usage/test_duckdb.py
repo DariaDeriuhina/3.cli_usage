79356f27ed67495c1b7ea549f3b860015e43256e
Daria.Deriuhina <Daria.Deriuhina@medeanalytics.com>
2024-08-28 05:39:59 -0700

    first commit

---
+++ 3.cli_usage/test_duckdb.py
@@ -0,0 +1,40 @@
+import duckdb
+import pytest
+
+def connect_to_db():
+    con = duckdb.connect('chinook.db')
+    return con
+
+def test_customers_from_usa():
+    con = connect_to_db()
+    query = "SELECT COUNT(*) FROM customers WHERE Country = 'USA';"
+    result = con.execute(query).fetchone()
+    assert result[0] == 13
+
+def test_customets_columns():
+    con = connect_to_db()
+    query = "PRAGMA table_info(customers)"
+    result = con.execute(query).fetchall()
+    column_names = [row[1] for row in result]
+    expected_columns = {'CustomerId', 'FirstName', 'LastName', 'Company', 'Address', 'City', 'State', 'Country', 'PostalCode', 'Phone', 'Fax', 'Email', 'SupportRepId'}
+    assert expected_columns.issubset(column_names), "Missing columns in customers"
+
+def test_customer_email_format():
+    con = connect_to_db()
+    query = "SELECT Email FROM customers WHERE Country = 'USA';"
+    results = con.execute(query).fetchall()
+    for email, in results:
+        assert '@' in email, "Incorrect format"
+
+def test_unique_cistomer_ids():
+    con = connect_to_db()
+    query = "SELECT COUNT(DISTINCT CustomerId) FROM customers;"
+    distinct_count = con.execute(query).fetchone()[0]
+    query = "SELECT COUNT(CustomerId) FROM customers;"
+    total_count = con.execute(query).fetchone()[0]
+    assert distinct_count == total_count, "There duplicates"
+
+def test_invalid_query():
+    con = connect_to_db()
+    with pytest.raises(Exception):
+        con.execute("FROM non_exist_table")
